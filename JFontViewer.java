f81b27c79793de54b7e1b4129c1c32c55a9e3eb6
jescubido <jarisse.escubido@gmail.com>
2023-03-02 13:32:17 -0800

    working components

--- JFontViewer.java
+++ JFontViewer.java
@@ -11,79 +11,181 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
+import javax.swing.event.*;
+import javax.swing.border.LineBorder;
 
  public class JFontViewer {
     
     // global variables
-    private JScrollBar horzScroll;
-    private JScrollPane fontScroll;
     private JList<String> fontList;
-    private JLabel phrase;
+    private JLabel text;
     
     JFontViewer()
     {
         JFrame frame = new JFrame("JFontViewer");
-        frame.setSize(500, 300);
+        frame.setSize(650, 300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         frame.setIconImage(new ImageIcon("JFontViewer.png").getImage());
 
-        // Create JPanel for size and horizontal scrollbar.
-        JPanel top = new JPanel(new GridLayout(0,1));
-        JLabel size = new JLabel("Size:");
-        horzScroll = new JScrollBar(Adjustable.HORIZONTAL);
-
-        top.add(size);
-        top.add(horzScroll);
-
 
         // Create JLabel for text to be changed.
-        JLabel text = new JLabel("the quick brown fox jumps over the lazy dog 0123456789");
+        text = new JLabel("the quick brown fox jumps over the lazy dog 0123456789");
+        text.setBorder(new LineBorder(Color.black));
+        text.setPreferredSize(new Dimension(600,50));
+        Font font = text.getFont();
+        text.setFont(new Font("Courier", Font.PLAIN, 8));
 
 
-        //Create JPanel for Fonts.
-        JPanel left = new JPanel(new GridLayout(0,1));
-        JLabel fonts = new JLabel("Fonts: ");
-        fontList = new JList(GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames());
+        // Create JPanel for size and horizontal scrollbar.
+        JPanel top = new JPanel(new GridLayout(0,1));
+        top.setPreferredSize(new Dimension(600,85));
+        top.setBorder(new LineBorder(Color.black));
+        JLabel size = new JLabel("Size:");
+        size.setDisplayedMnemonic('S');
 
-        left.add(fonts);
-        left.add(fontList);
+        JSlider horzSlider = new JSlider(JSlider.HORIZONTAL, 8,20,8);
+        horzSlider.setPaintTicks(true);
+        horzSlider.setPaintLabels(true);
+        horzSlider.setMajorTickSpacing(2);
+        horzSlider.setSnapToTicks(true);
+        
+        horzSlider.addChangeListener(new ChangeListener()
+        {
+            public void stateChanged(ChangeEvent ce)
+            {
+                text.setFont(new Font(
+                text.getFont().getName(),
+                text.getFont().getStyle(),
+                horzSlider.getValue()
+                ));
+            }
+        });
 
-        // Set single-selection mode and selection listener for fontList.
+        top.add(size);
+        top.add(horzSlider);
+
+
+        // Create JPanel for Fonts.
+        JPanel left = new JPanel(new FlowLayout());
+        left.setPreferredSize(new Dimension(200,100));
+        left.setBorder(new LineBorder(Color.black));
+        JLabel fonts = new JLabel("Fonts:");
+        fonts.setAlignmentX(JComponent.LEFT_ALIGNMENT);
+        fonts.setSize(50,50);
+        fonts.setDisplayedMnemonic('F');
+        // Create font list with vertical slider and single selection.
+        fontList = new JList<>(GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames());
+        JScrollPane vertScroll = new JScrollPane(fontList);
+        vertScroll.getVerticalScrollBar().setFocusable(true);
+        fontList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        fontList.addListSelectionListener(new ListSelectionListener()
+        {
+            public void valueChanged(ListSelectionEvent ce)
+            {
+                int index = fontList.getSelectedIndex();
+                if (index != -1)
+                {
+                    text.setFont(new Font(
+                        fontList.getSelectedValue(),
+                        text.getFont().getStyle(),
+                        horzSlider.getValue()
+                    ));
+                }
+            }
+        });
+        
+        
+        /**fontList.addListSelectionListener(ll ->
+        {
+            if(ll.getValueIsAdjusting())
+            {
+                fontName = fontList.getSelectedValue();
+                text.setFont(new Font(
+                    text.getFont().getSelectedValue(),
+                    text.getFont().getStyle(),
+                    horzSlider.getValue()
+                    ));
+            }
+        });
+        */
+        // Edit preview text when font is clicked.
         
 
-
+        left.add(fonts);
+        left.add(vertScroll);
+        
 
         // Create JPanel for Style.
         JPanel center = new JPanel(new GridLayout(0,1));
-        JLabel style = new JLabel("Style: ");
-        JRadioButton regular = new JRadioButton("Regular", true);
-        JRadioButton italic = new JRadioButton("Italic");
-        JRadioButton bold = new JRadioButton("Bold");
+        center.setPreferredSize(new Dimension(200,100));
+        center.setBorder(new LineBorder(Color.black));
+        JLabel style = new JLabel("Style:");
+        JRadioButton regularStyle = new JRadioButton("Regular", true);
+        regularStyle.setMnemonic('R');
+        regularStyle.addActionListener(al ->
+        {
+            text.setFont(new Font(
+                text.getFont().getName(),
+                Font.PLAIN,
+                horzSlider.getValue()
+                ));
+        });
+        JRadioButton italicStyle = new JRadioButton("Italic");
+        italicStyle.setMnemonic('I');
+        italicStyle.addActionListener(al ->
+        {
+            text.setFont(new Font(
+                text.getFont().getName(),
+                Font.ITALIC,
+                horzSlider.getValue()
+                ));
+        });
+        JRadioButton boldStyle = new JRadioButton("Bold");
+        boldStyle.setMnemonic('B');
+        boldStyle.addActionListener(al ->
+        {
+            text.setFont(new Font(
+                text.getFont().getName(),
+                Font.BOLD,
+                horzSlider.getValue()
+                ));
+        });
         ButtonGroup styleType = new ButtonGroup();
 
-        styleType.add(regular);
-        styleType.add(italic);
-        styleType.add(bold);
+
+        styleType.add(regularStyle);
+        styleType.add(italicStyle);
+        styleType.add(boldStyle);
         center.add(style);
-        center.add(regular);
-        center.add(italic);
-        center.add(bold);
+        center.add(regularStyle);
+        center.add(italicStyle);
+        center.add(boldStyle);
 
 
 
 
         // Create JPanel for Effects.
         JPanel right = new JPanel(new GridLayout(0,1));
-        JLabel effects = new JLabel("Effects: ");
+        right.setPreferredSize(new Dimension(200,100));
+        right.setBorder(new LineBorder(Color.black));
+        JLabel effects = new JLabel("Effects:");
+        effects.setDisplayedMnemonic('E');
         JCheckBox allCaps = new JCheckBox("All caps", false);
-        ButtonGroup rightBtnGrp = new ButtonGroup();
+        allCaps.setMnemonic('c');
 
-        rightBtnGrp.add(allCaps);
+        allCaps.addActionListener((ae) ->
+        {
+            if(allCaps.isSelected()) 
+                text.setText(text.getText().toUpperCase());
+            else
+                text.setText(text.getText().toLowerCase());
+        });
+        
+
         right.add(effects);
         right.add(allCaps);
+
 
 
         // Add components to content pane.
@@ -100,7 +202,7 @@ import javax.swing.event.ListSelectionListener;
 
     public static void main(String[] args)
     {
-        SwingUtilities.invokeLater(() -> new JFontViewer());;
+        SwingUtilities.invokeLater(() -> new JFontViewer());
     }
 
  }
