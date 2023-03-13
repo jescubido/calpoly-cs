b3b68993a988a9595d272b8ca5e9d9fd833b0d77
jescubido <102789017+jescubido@users.noreply.github.com>
2023-03-13 01:35:14 -0700

    Update FileViewer.java

--- FileViewer.java
+++ FileViewer.java
@@ -5,17 +5,17 @@
  * Course:  cs-2450-01-sp23
  * 
  * Description:
- *          A Swing application that opens a file.
+ *          A Swing application that edits the contents of a file. 
  */
 
  import java.awt.*;
  import java.awt.event.*;
-import javax.swing.*;
+ import javax.swing.*;
  import javax.swing.border.EtchedBorder;
 
  public class FileViewer
  {
-    JTextArea text;
+    private JTextArea text;
 
     FileViewer()
     {
@@ -28,61 +28,80 @@ import javax.swing.*;
         text.setFont(new Font("Courier New",Font.PLAIN,12));  
         
         
-        
-        // Create a menu bar.
-        JMenuBar menuBar = new JMenuBar();
+        JMenuBar menuBar = new JMenuBar(); // Create a menu bar.
 
-        // Create Menus.
+        /**
+         * File Menu: 
+         * Exit MenuItem simply exits the program.
+        */
         JMenu file = new JMenu("File");
-        JMenu format = new JMenu("Format");
-        JMenu help = new JMenu("Help");
+        file.setMnemonic(KeyEvent.VK_F);
 
-        // Components for File Menu
         JMenuItem exit = new JMenuItem("Exit");
+        exit.setMnemonic(KeyEvent.VK_E);
         exit.addActionListener(ae -> System.exit(0)); // Exit program when clicked.
-        file.add(exit);
-
-        // Components for Format Menu
-        JMenuItem wordwrap = new JMenuItem("Word Wrap");
-        JMenu font = new JMenu("Font"); // Create Font submenu
-        JMenu color = new JMenu("Color");
-
-        wordwrap.addActionListener(ae -> {text.setLineWrap(true);}); // wraps word across screen
         
-        // Components for Font submenu.
-        JMenuItem larger = new JMenuItem("Larger");
+        file.add(exit); // Add component to content pane.
+
+
+        /**
+         * Format Menu:
+         * First MenuItem allows for word to be in next line once it reaches the end of the screen.
+         * Font Submenu changes the size of the font larger or smaller in increments of 2.
+         * Color Submenu changes the color of the background and foreground w/ three preset settings.
+        */ 
+        JMenu format = new JMenu("Format");
+        format.setMnemonic(KeyEvent.VK_O);
+
+        JMenuItem wordwrap = new JMenuItem("Word Wrap", KeyEvent.VK_W);
+        wordwrap.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_W, InputEvent.CTRL_MASK));
+        wordwrap.addActionListener(ae -> {text.setLineWrap(true);}); // Create newline when word reaches end of screen.
+
+
+        JMenu font = new JMenu("Font"); // Create Font submenu
+        font.setMnemonic(KeyEvent.VK_F);
+
+
+        JMenuItem larger = new JMenuItem("Larger", KeyEvent.VK_L); // Create Larger MenuItem and increase font size by 2.
+        larger.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_PLUS, InputEvent.CTRL_MASK));
+
         larger.addActionListener(ae ->
         {
             int fontSize = text.getFont().getSize();
-            if(fontSize < 20)
+            if(fontSize < 20) // Maximum size to increase font is 20.
             {
                 fontSize += 2;
                 text.setFont(new Font("Courier New", Font.PLAIN, fontSize));
             }
         });
         
-        JMenuItem smaller = new JMenuItem("Smaller");
+        JMenuItem smaller = new JMenuItem("Smaller"); // Create Smaller MenuItem and decrease font size by 2.
+        smaller.setMnemonic(KeyEvent.VK_S);
+        smaller.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_MINUS, InputEvent.CTRL_MASK));;
         smaller.addActionListener(ae -> 
         {
             int fontSize = text.getFont().getSize();
-            if(fontSize > 10)
+            if(fontSize > 10) // Maximum size to decrease font is 10.
             {
                 fontSize -= 2;
                 text.setFont(new Font("Courier New", Font.PLAIN, fontSize));
             }
         });
-        
-        font.add(larger);
-        font.add(smaller);
 
-        // Create components for Color submenu.
+
+        JMenu color = new JMenu("Color"); // Create Color Submenu.
+        color.setMnemonic(KeyEvent.VK_C);
+
         JMenuItem defaultColor = new JMenuItem("Default");
+        
+        defaultColor.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_D, InputEvent.CTRL_MASK));;
         defaultColor.addActionListener(ae ->
         {
             text.setBackground(Color.WHITE);
             text.setForeground(Color.BLACK);
         });
         JMenuItem ibmColor = new JMenuItem("IBM");
+        ibmColor.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_1, InputEvent.CTRL_MASK));;
         ibmColor.addActionListener(ae ->
         {
             Color navyBlue = new Color(0,0,128);
@@ -90,35 +109,47 @@ import javax.swing.*;
             text.setForeground(Color.WHITE);
         });
         JMenuItem windowColor = new JMenuItem("Window");
+        windowColor.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_2, InputEvent.CTRL_MASK));;
         windowColor.addActionListener(ae ->
         {
             text.setBackground(Color.BLACK);
             text.setForeground(Color.WHITE);
         });
 
+        
+        // Adding components to menus and content pane.
+        font.add(larger);
+        font.add(smaller);        
+
         color.add(defaultColor);
-        color.addSeparator();
+        color.addSeparator(); // Adds Line Separator to Format Menu.
         color.add(ibmColor);
         color.add(windowColor);
 
-        // Add components to Format Menu.
         format.add(wordwrap);
-        format.add(font); // Add font as submenu to Format Menu
-        format.add(color);
+        format.add(font); // Add Font Submenu to Format Menu
+        format.add(color); // Add Color Submenun to Format Menu
 
-        // Create components for Help Menu.
+
+        /**
+         * Create Help Menu:
+         * About MenuItem displays my name in the NORTH for 5 seconds.
+         */
+        JMenu help = new JMenu("Help");
+        help.setMnemonic(KeyEvent.VK_H);
+
         JMenuItem about = new JMenuItem("About");
+        about.setMnemonic(KeyEvent.VK_A);
         JLabel name = new JLabel("(c) Jarisse Escubido");
                     
+        help.add(about); // Add component to Help Menu.
+        
 
-        help.add(about);
-
-        // Add menus to menu bar.
+        // Add menus to menu bar and to content pane.
         menuBar.add(file);
         menuBar.add(format);
         menuBar.add(help);
 
-        // Add components to the content pane.
         frame.setJMenuBar(menuBar);
         frame.add(text, BorderLayout.CENTER);
 
