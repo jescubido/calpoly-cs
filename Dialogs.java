8e3844c6e1975f80f3f090a14c70f8a921ed6525
jescubido <jarisse.escubido@gmail.com>
2023-04-19 22:56:35 -0700

    JFontViewer classes

---
+++ Dialogs.java
@@ -0,0 +1,9 @@
+/**
+ * Name: Escubido, Jarisse
+ * Homework: #2
+ * Due: 21 April 2023
+ * Course: cs-2450-01-sp23
+ * 
+ * Description:
+ *      Implementing an application that previews a phrase after font, style, or effects are added.
+ */
