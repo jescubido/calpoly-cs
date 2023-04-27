d486c30c24feb0325edcdfb14992b3a6661c34ba
jescubido <jarisse.escubido@gmail.com>
2023-04-27 15:21:56 -0700

    imageIcon for JNotepad

---
+++ finalProject/Dialogs.java
@@ -0,0 +1,31 @@
+package finalProject;
+/**
+ * Name: Escubido, Jarisse
+ * Project: #6
+ * Due: 12 May 2023
+ * Course: cs-2450-01-sp23
+ * 
+ * Description:
+ *      Implement a version of the Windows Notepad application.
+ */
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Dialogs
+{
+    // initializing variables
+
+
+    Dialogs()
+    {
+
+
+    }
+
+    public static void main(String[] args)
+    {
+    SwingUtilities.invokeLater(() -> new Dialogs());
+    }
+}
