9db011b66a2befa9d2307e7742013b1264012fab
jescubido <jarisse.escubido@gmail.com>
2023-04-28 02:10:57 -0700

    initial commit; problems with event handling

--- finalProject/Dialogs.java
+++ finalProject/Dialogs.java
@@ -11,21 +11,53 @@ package finalProject;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
 
 public class Dialogs
 {
-    // initializing variables
+	public class JFontChooser
+	{
+		static Font font;
 
+		static Font showDialog(JFrame frame, String title, Font initialFont)
+		{
+			JDialog dlg = new JDialog(frame, "Choose Font", true);
+			dlg.setSize(100, 150);
+			dlg.setLayout(new FlowLayout());
+			dlg.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
-    Dialogs()
-    {
+			/*
+			* format the dialog as the frame in project 3
+			* use initialFont to set the selected font, size, and style
+			* Add Ok and Cancel buttons under the sample text
+			*/
 
+			// Create Ok and Cancel buttons.
+			JButton ok = new JButton("Ok");
+			ok.addActionListener((ae) -> {
+				Font font = JFontChooser.showDialog(null, "Select Font", null);
+				dlg.dispose();
+			});
+			dlg.add(ok);
 
-    }
+			JButton cancel = new JButton("Cancel");
+			cancel.addActionListener((ae) -> {
+				dlg.dispose();
+			});
+			dlg.add(cancel);
 
-    public static void main(String[] args)
-    {
-    SwingUtilities.invokeLater(() -> new Dialogs());
-    }
+			// Create panel to add buttons.
+			JPanel panel = new JPanel();
+			panel.add(ok);
+			panel.add(cancel);
+
+			// Add components to content pane.
+			dlg.add(panel, BorderLayout.SOUTH);
+
+			font = null;
+			dlg.setLocationRelativeTo(frame);
+			dlg.setVisible(true);
+
+			return font;
+		}
+	}
 }
